adc2390bdc434b333371dcdd94172fae789df04c
Akhilesh K <akhilesh.k@statocean.co>
2026-01-13 16:47:50 +0530

    sample web app

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	
+	port := ":8080"
+	log.Printf("Server starting on port %s", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
